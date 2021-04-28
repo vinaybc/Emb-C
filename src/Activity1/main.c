51c7b76143af35d611faefa7b9fe20bc78d9e9dd
vinaybc <65846052+vinaybc@users.noreply.github.com>
2021-04-28 12:03:12 +0530

    Update main.c

--- src/Activity1/main.c
+++ src/Activity1/main.c
@@ -1,28 +1,59 @@
 #include<avr/io.h>
-int main()
+#include<util/delay.h>
+#define F_CPU 16000000UL 	/**< to Define the Clock Frequency as 16 MHz */
+#define HIGH 	(1)			/**< GPIO state HIGH */
+#define LOW 	(0)			/**< GPIO state LOW */
+
+void setup_io_pins(void)
 {
-    DDRB |= (1<<PB0); /* configuring port B as output */
-    DDRD |= (0<<PD2);
-    DDRD |= (0<<PD4);/* configure PD2 and PD4 of port D as input */
-    PORTB &= ~(1<<PB0); /*write 0 to port B */
-    while(1){
-    if(PIND & 1<<PD2)/*PIND is Used for gpio read */
-    {
-        if(PIND & 1<<PD4)
-        {
-            PORTB |= (1<<PB0);/*if both buttons are selected led turns on*/
-        }
-        else
-        PORTB &= ~(1<<PB0);
-    }
-    else
-        PORTB &= ~(1<<PB0);
-    }
-    return 0;
+	//DDRB |= (1 << DDB0);
+	DDRB |=(1<<PB0); // B0=1 for led
+	DDRD&=~(1<<PD0); // clear bit
+	PORTD|=(1<<PD0);
+	DDRD&=~(1<<PD1); // clear bit
+	PORTD|=(1<<PD1);
+
 }
 
+void set_gpio_state(uint8_t port, uint8_t pin, uint8_t state)
+{
+	port = (HIGH << pin);
+}
 
+void delay_ms(unsigned int delaytime)
+{
+	int i = 0;
+	for (i = 0; i <= delaytime; i++)
+	{
+		_delay_ms(1000); // delay in ms
+	}
+}
 
+/**
+ * @brief Main Function where the Code execution starts
+ * 
+ * @return int Return 0 if the program completes successfully
+ * @note PORTB0 is in sink config. i.e when pin is Low, the LED will turn OFF
+ * @note PORTB0 is in sink config. i.e when pin is High, the LED will turn ON
+ */
+int main(void)
+{
+	// setup the leds
+	setup_io_pins();
 
-
-
+	while (1)
+	{	
+		if(!(PIND&(1<<PD0))&&(!(PIND&(1<<PD1))))//switch press
+ 	        {
+       		 PORTB|=(1<<PB0);
+     		}
+     		else{
+       		 PORTB&=~(1<<PB0);
+		}
+		/*set_gpio_state(PORTB, PORTB0, HIGH);
+		delay_ms(1000);
+		set_gpio_state(PORTB, PORTB0, LOW);
+		delay_ms(500);*/
+	}
+	return 0;
+}
