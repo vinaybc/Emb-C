9d390a41abaa9b8e62f67e1198751ece1a7ecfe7
vinaybc <65846052+vinaybc@users.noreply.github.com>
2021-04-30 14:50:31 +0530

    Update activity3.h

--- inc/activity3.h
+++ inc/activity3.h
@@ -1,21 +1,29 @@
+#ifndef _ACTIVITY3_H_
+#define _ACTIVITY3_H_
+
 /**
  * @file activity3.h
  * @author Vinay B C
- * @brief 
+ * @brief Activity3 in the embedded case study
  * @version 0.1
- * @date 2021-04-29
+ * @date 2021-04-23
  * 
  * @copyright Copyright (c) 2021
  * 
- **/
-#ifndef __activity3_h__
-#define __activity3_h__
-#include<avr/io.h>
-/**
- * @brief Function definition
- * 
- * @param temp_value 
  */
-void OutPWM(uint16_t temp_value);
-void InitPWM(void);
-#endif
+
+
+/**
+ * @brief Function to initialize PWM    
+ * 
+ */
+void InitializePWM(void);
+
+/**
+ * @brief Function to Generate PWM    
+ * 
+ * @param[in] Temperature input temperature obtained from ADC 
+ * @return TempToCAN
+ */
+char GeneratePWM(uint16_t);
+#endif  /* _ACTIVITY3_H_ */
