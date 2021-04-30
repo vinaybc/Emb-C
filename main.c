2764058944e47aca164e2982624434abfd72f0c3
vinaybc <65846052+vinaybc@users.noreply.github.com>
2021-04-30 12:39:16 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,29 +1,63 @@
-#include <avr/io.h>
-#include<util/delay.h>
+/**
+ * @file main.c
+ * @author nuPURohit (https://github.com/nuPURohit/Embedded_C_LTTS.git)
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-28
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include "activity1.h"
+#include "activity2.h"
+#include "activity3.h"
+#include<avr/io.h>
+
+
+/**
+ * @brief Initialize all the peripherals
+ * 
+ */
+
+void peripheral_init(void)
+{
+    /*Configure LED and Switch pins*/
+    InitLED();
+    /*Configure ADC registers and pins*/
+    InitADC();
+    /*Configure PWM registers and pins*/
+    InitPWM();
+}
+    
+   
+uint16_t temp;
+
 int main(void)
 {
-
-    DDRB|=(1<<PB0);
-    DDRB&=~(2<<PD0);
-    DDRB&=~(4<<PD0);
-    PORTD|=(2<<PD0);
-    PORTD|=(4<<PD0);
-
+    /*uint16_t temp;*/
+    // Initialize peripherals
+    peripheral_init();
     while(1)
     {
-        if(!(PIND&(2<<PD0))&& !(PIND & !(PIND & (4<<PD0))))
+        if(SENSOR_ON) //If switch_1 is ON
         {
-
-            PORTB|=(1<<PB0);
-            _delay_ms(2000);
-
+            if(HEAT_ON) //If switch_2 is ON
+            {
+                ledstat(LED_ON);//LED is ON
+                temp=ReadADC(0);
+                OutPWM(temp);
+            }
+            else
+            {
+                
+                ledstat(LED_OFF);
+            }
         }
         else
         {
-            PORTB&=~(1<<PB0);
-            _delay_ms(2000);
+            ledstat(LED_OFF);//LED is OFF
+            OCR1A=0;
         }
     }
-
     return 0;
 }
