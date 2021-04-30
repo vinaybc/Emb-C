98e7210fc2a0867e61e4be033ad1de58b16284b1
vinaybc <65846052+vinaybc@users.noreply.github.com>
2021-04-30 12:12:30 +0530

    Update Activity1.h

--- inc/Activity1.h
+++ inc/Activity1.h
@@ -1,33 +1,87 @@
 /**
- * @file Activity1.h
+ * @file activity1.h
+ * @author /**
+ * @file activity1.h
  * @author Vinay B C
- * @brief Project to check whether the person has seated and turned on the Temperature sensor by indicating it on LED
+ * @brief 
  * @version 0.1
- * @date 2021-04-28
+ * @date 2021-04-24
  * 
  * @copyright Copyright (c) 2021
  * 
  */
-
-/**
- * Macro Definitions
- */
-#define F_CPU 16000000UL 	/**< to Define the Clock Frequency as 16 MHz */
-#define HIGH 	(1)			/**< GPIO state HIGH */
-#define LOW 	(0)			/**< GPIO state LOW */
+#ifndef _activity1_h_
+#define _activity1_h_
 
 /**
  * Include files
  */ 
 #include <avr/io.h>
-#include <util/delay.h>
+#include "activity1.h"
 
+//Macro definition 
+
+#define LED_ON 	(0x01)			/**< LED state HIGH */
+#define LED_OFF	(0x00)			/**< LED state LOW */
+
+
+#define LED_PORT (PORTB)    /**< LED Port Number */
+#define LED_PIN  (PORTB0)   /**< LED Pin number  */
+#define SENSOR_ON !(PIND&(1<<PD0)) // Switch1 ON
+#define HEAT_ON !(PIND&(1<<PD1)) // Switch2 ON
 
 /**
  * Function Definitions
  */
 
 /**
- * @brief Function to configure the IO Pins for input and output
+ * @brief Change the state of the LED Pin according to the value of state
+ * 
+ * @param state Pin level to which the LED Pin should be set
  */
-void setup_io_pins(void);
+
+void ledstat(uint8_t state);
+void InitLED(void);
+
+#endif /** __activity1_h_ */i
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-24
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef _activity1_h_
+#define _activity1_h_
+
+/**
+ * Include files
+ */ 
+#include <avr/io.h>
+#include "activity1.h"
+
+//Macro definition 
+
+#define LED_ON 	(0x01)			/**< LED state HIGH */
+#define LED_OFF	(0x00)			/**< LED state LOW */
+
+
+#define LED_PORT (PORTB)    /**< LED Port Number */
+#define LED_PIN  (PORTB0)   /**< LED Pin number  */
+#define SENSOR_ON !(PIND&(1<<PD0)) // Switch1 ON
+#define HEAT_ON !(PIND&(1<<PD1)) // Switch2 ON
+
+/**
+ * Function Definitions
+ */
+
+/**
+ * @brief Change the state of the LED Pin according to the value of state
+ * 
+ * @param state Pin level to which the LED Pin should be set
+ */
+
+void ledstat(uint8_t state);
+void InitLED(void);
+
+#endif /** __activity1_h_ */
