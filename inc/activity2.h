55f4ce3226d292b203d09b7cdf55e1c8fb5d1943
vinaybc <65846052+vinaybc@users.noreply.github.com>
2021-04-30 14:49:55 +0530

    Update activity2.h

--- inc/activity2.h
+++ inc/activity2.h
@@ -1,22 +1,28 @@
+#ifndef _ACTIVITY2_H_
+#define _ACTIVITY2_H_
+
 /**
  * @file activity2.h
  * @author Vinay B C
- * @brief 
+ * @brief Activity2 in the embedded case study
  * @version 0.1
- * @date 2021-04-28
+ * @date 2021-04-23
  * 
  * @copyright Copyright (c) 2021
  * 
  */
-#ifndef _activity2_h_
-#define _activity2_h_
+
 
 /**
- * @brief Reading the analog input and returning a digital 8bit value
+ * @brief Function to initialize ADC
  * 
- * @param ch 
- * @return uint16_t 
  */
-uint16_t ReadADC(uint8_t ch);
-void InitADC(); 
-#endif /** __activity2_h_ */
+void InitializeADC();
+
+/**
+ * @brief Function to read values from a channel 
+ * @param[in] channel channel number
+ */
+uint16_t ReadADC(uint8_t channel);
+
+#endif /* _ACTIVITY2_H_ */
