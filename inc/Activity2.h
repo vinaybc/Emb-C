d2ed67ae132498cbcbff6a48934adc8e6f637e0a
vinaybc <65846052+vinaybc@users.noreply.github.com>
2021-04-30 12:13:20 +0530

    Update Activity2.h

--- inc/Activity2.h
+++ inc/Activity2.h
@@ -1,7 +1,22 @@
-#ifndef __ACTIVITY2_H_
-#define __ACTIVITY2_H_
+/**
+ * @file activity2.h
+ * @author Vinay B C
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-28
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef _activity2_h_
+#define _activity2_h_
 
-#include <avr/io.h>
-#include <util/delay.h>
-
-#endif /** __ACTIVITY2_H_ */
+/**
+ * @brief Reading the analog input and returning a digital 8bit value
+ * 
+ * @param ch 
+ * @return uint16_t 
+ */
+uint16_t ReadADC(uint8_t ch);
+void InitADC(); 
+#endif /** __activity2_h_ */
