bb5a3d4a6c559e17e6d9a6049f852bc3b3f51f8e
vinaybc <65846052+vinaybc@users.noreply.github.com>
2021-04-30 14:49:07 +0530

    Update activity1.h

--- inc/activity1.h
+++ inc/activity1.h
@@ -1,44 +1,82 @@
+#ifndef _ACTIVITY1_H_
+#define _ACTIVITY1_H_
 /**
  * @file activity1.h
- * @author Vinay  B C
- * @brief 
+ * @author Vinay B C
+ * @brief Activity1 in the embedded case study
  * @version 0.1
- * @date 2021-04-24
+ * @date 2021-04-23
  * 
  * @copyright Copyright (c) 2021
  * 
  */
-#ifndef _activity1_h_
-#define _activity1_h_
-
 /**
  * Include files
  */ 
+#define F_CPU 16000000UL 	/**< Clock Frequency of MCU is 16 MHz */
 #include <avr/io.h>
-#include "activity1.h"
+#include <util/delay.h>
 
-//Macro definition 
+/**
+ * Macro Definitions
+ */
+
 
 #define LED_ON 	(0x01)			/**< LED state HIGH */
 #define LED_OFF	(0x00)			/**< LED state LOW */
-
-
 #define LED_PORT (PORTB)    /**< LED Port Number */
 #define LED_PIN  (PORTB0)   /**< LED Pin number  */
-#define SENSOR_ON !(PIND&(1<<PD0)) // Switch1 ON
-#define HEAT_ON !(PIND&(1<<PD1)) // Switch2 ON
-
+#define BUTTON_SENSOR_PORT  (PORTD) /**< BUTTON_SENSOR Port number  */
+#define HEATER_SENSOR_PORT  (PORTD) /**< HEATER_SENSOR Port number  */
+#define BUTTON_SENSOR_PIN  (PORTD0) /**< BUTTON_SENSOR Pin number  */
+#define HEATER_SENSOR_PIN  (PORTD1) /**< HEATER_SENSOR Pin number  */
+#define BUTTON_SENSOR_ON 	!(PIND & 1<<PD0)	/**< BUTTON_SENSOR state HIGH */
+#define HEATER_SENSOR_ON	!(PIND & 1<<PD1)	/**< HEATER_SENSOR state HIGH */
+#define SET_PORTB0_AS_OUTPUT  DDRB |= (1<<PORTB0)
+#define SET_PD0_AND_PD1_AS_PULLUP  PORTD |= (1<<PORTD1)|(1<<PORTD0)
+#define SET_PORTD_AS_INPUT  DDRD = 0x00
 /**
- * Function Definitions
- */
-
-/**
- * @brief Change the state of the LED Pin according to the value of state
+ * @brief Function to change status of LED
  * 
- * @param state Pin level to which the LED Pin should be set
+ * @param[in] state ON or OFF
+ * 
+ */
+void ChangeLEDState(uint8_t state);
+
+/**
+ * @brief Function to set pin of ButtonSensor
+ * 
+ * 
+ */
+void SetButtonSensorPin(void);
+
+/**
+ * @brief Function to set pin of HeaterSensor
+ * 
+ * 
+ */
+void SetHeaterSensorPin(void);
+
+/**
+ * @brief Function to generate delay in micro seconds
+ * 
+ * @param[in] delay_time Delay in Micro seconds
+ * 
+ */
+void DelayMilliSecond(uint32_t delay_time);
+
+/**
+ * @brief Function to change LED status according to the user requirement in activity1
+ * 
+ * @return FLAG
+ */
+uint8_t StatusOfLedActuator(void);
+
+/**
+ * @brief Function to initialize peripherals of microcontroller
+ * 
  */
 
-void ledstat(uint8_t state);
-void InitLED(void);
+void InitializePeripherals(void);
 
-#endif /** __activity1_h_ */
+#endif /* _ACTIVITY1_H_ */
