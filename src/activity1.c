d9d7e58063c34c0f160949c4371152109d404522
vinaybc <65846052+vinaybc@users.noreply.github.com>
2021-04-30 12:16:45 +0530

    Update and rename Activity1.c to activity1.c

---
+++ src/activity1.c
@@ -0,0 +1,30 @@
+  
+/**
+ * @file activity1.c 
+ * @author nuPURohit (https://github.com/nuPURohit/Embedded_C_LTTS.git)
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-24
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+// include files
+#include<avr/io.h>
+#include "activity1.h"
+/**
+ * @brief Change the state of the LED Pin according to the value of state
+ * 
+ * @param state Pin level to which the LED Pin should be set
+ */
+void ledstat(uint8_t state)
+{
+	LED_PORT = (state << LED_PIN);
+}
+void InitLED(void)
+{
+	DDRB|=(1<<PB0);
+    DDRD&=~(1<<PD0);
+    PORTD|=(1<<PD0);
+    PORTD|=(1<<PD1);
+}
